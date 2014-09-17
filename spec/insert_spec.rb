5ae0856988530bc0e27f23d309866df647c23fa6
ahimmelstoss <ahimmelstoss@gmail.com>
2014-09-17 11:50:43 -0400

    insert

--- spec/insert_spec.rb
+++ spec/insert_spec.rb
@@ -1,4 +1,4 @@
-describe 'populating the bears table' do
+describe 'populating the bears table' do 
   before do
     @db = SQLite3::Database.new(':memory:')
     @sql_runner = SQLRunner.new(@db)
@@ -6,11 +6,7 @@ describe 'populating the bears table' do
     @sql_runner.execute_insert_file
   end
 
-  it 'has 8 bears' do
-    expect(@db.execute("SELECT COUNT(*) FROM bears;").flatten[0]).to eq(8)
-  end
-  
-  it 'has an unnamed bear' do
-    expect(@db.execute("SELECT COUNT(*) FROM bears WHERE name IS NULL;").flatten[0]).to eq(1)
+  it 'has 15 bears' do 
+    expect(@db.execute("SELECT COUNT(*) FROM bears;").flatten[0]).to eq(15)
   end
 end
