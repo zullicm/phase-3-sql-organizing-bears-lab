3a70a6c3275987e90d639dfb932692ee2f2af4fd
ahimmelstoss <ahimmelstoss@gmail.com>
2014-09-17 11:54:49 -0400

    edit runner and select spec

--- bin/sql_runner.rb
+++ bin/sql_runner.rb
@@ -1,5 +1,4 @@
 require_relative 'environment'
-require 'pry'
 
 class SQLRunner
   def initialize(db)
@@ -8,21 +7,29 @@ class SQLRunner
 
   def execute_create_file
     sql = File.read("lib/create.sql")
-    @db.execute_batch(sql)
+    execute_sql(sql)
   end
 
   def execute_insert_file
     sql = File.read("lib/insert.sql")
-    @db.execute_batch(sql)
+    execute_sql(sql)
   end
 
   def execute_select_file
     sql = File.read("lib/select.sql")
-    @db.execute_batch(sql)
+    execute_sql(sql)
   end
 
-  def execute_data
-    sql = File.read('lib/seed.sql')
-    @db.execute_batch(sql)
+  def execute_encoded_data
+
+    encoded_data = ""
+    decoded_data = Base64.decode64(encoded_data)
+    sql = File.open('lib/decoded_data.sql', 'w'){ |f| f.write (decoded_data) }
+    sql = File.read('lib/decoded_data.sql')
+    execute_sql(sql)
+  end
+
+  def execute_sql(sql)
+     sql.scan(/[^;]*;/m).each { |line| @db.execute(line) } unless sql.empty?
   end
 end
