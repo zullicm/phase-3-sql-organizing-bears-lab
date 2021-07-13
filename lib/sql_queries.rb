f09731bc1419a523dcd7872b474423ba681c3836
ihollander <ianholla@gmail.com>
2021-07-13 17:40:04 -0400

    Added solution

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,19 +1,51 @@
-def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+def selects_all_female_bears_return_name_and_age 
+  <<-SQL
+    SELECT 
+      bears.name, 
+      bears.age 
+    FROM bears 
+    WHERE sex='F';
+  SQL
 end
 
-def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+def selects_all_bears_names_and_orders_in_alphabetical_order 
+  <<-SQL
+    SELECT 
+      bears.name 
+    FROM bears 
+    ORDER BY bears.name ASC;
+  SQL
 end
 
-def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest 
+  <<-SQL
+    SELECT 
+      bears.name, 
+      bears.age 
+    FROM bears 
+    WHERE alive=1 
+    ORDER BY bears.age ASC;
+  SQL
 end
 
-def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+def selects_oldest_bear_and_returns_name_and_age 
+  <<-SQL
+    SELECT 
+      bears.name, 
+      bears.age 
+    FROM bears 
+    ORDER BY bears.age DESC 
+    LIMIT 1;
+  SQL
 end
 
-def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+def select_youngest_bear_and_returns_name_and_age 
+  <<-SQL
+    SELECT 
+      bears.name, 
+      bears.age 
+    FROM bears 
+    ORDER BY bears.age ASC 
+    LIMIT 1;
+  SQL
 end
